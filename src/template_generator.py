b5cc579c33c1227d53b7ff7a53ef377c988b7878
= <=>
2023-04-17 20:24:50 -0700

    example usage

--- src/template_generator.py
+++ src/template_generator.py
@@ -1,18 +1,18 @@
 import pystache
 import os
 
-def init_project():
-    project_name = input("Enter a name for your project: ")
-    
-    # Create the main project directory
+def init_project(project_name):
+    if project_name == "":
+        project_name = input("Enter project name: ")
     os.mkdir(project_name)
-    print(f"Created project directory: {project_name}")
-    
-    # Create subdirectories for each generatable item type
-    subdirectories = ["schemas", "path_objects", "parameters", "info", "headers", "media_types", "responses", "parameter_objects", "tags", "servers", "contact", "license"]
-    for subdir in subdirectories:
-        os.mkdir(os.path.join(project_name, subdir))
-        print(f"Created {subdir} directory")
+    os.mkdir(os.path.join(project_name, "schemas"))
+    os.mkdir(os.path.join(project_name, "paths"))
+    os.mkdir(os.path.join(project_name, "parameters"))
+    os.mkdir(os.path.join(project_name, "responses"))
+    os.mkdir(os.path.join(project_name, "tags"))
+    os.mkdir(os.path.join(project_name, "servers"))
+    os.mkdir(os.path.join(project_name, "contacts"))
+    os.mkdir(os.path.join(project_name, "licenses"))
 
 def render_template(template_file, context, output_file):
     with open(template_file, 'r') as f:
@@ -74,66 +74,81 @@ def generate_license(context, output_file='license.yaml', version='v2.0'):
     check_version(version)
     render_template(f"templates/{version}/license.mustache", context, output_file)
 
+def demo():
+    # Example usage
+    project_name = "demo"
+    init_project(project_name)
+    schema_context = {
+        'schemaName': 'Book',
+        'type': 'object',
+        'properties': [
+            {'name': 'title', 'type': 'string', 'description': 'The title of the book', 'example': 'The Catcher in the Rye'},
+            {'name': 'author', 'type': 'string', 'description': 'The author of the book', 'example': 'J.D. Salinger'},
+            {'name': 'publication_date', 'type': 'string', 'description': 'The publication date of the book', 'example': '1951-07-16', 'format': 'date'}
+        ],
+        'required': ['title', 'author'],
+        'description': 'A book in the library.'
+    }
 
+    generate_schema(schema_context, os.path.join(f"{project_name}/schemas", 'book.yaml'), version='v3.0')
 
-# Example usage
-schema_context = {
-    'schemaName': 'Book',
-    'type': 'object',
-    'properties': [
-        {'name': 'title', 'type': 'string', 'description': 'The title of the book', 'example': 'The Catcher in the Rye'},
-        {'name': 'author', 'type': 'string', 'description': 'The author of the book', 'example': 'J.D. Salinger'},
-        {'name': 'publication_date', 'type': 'string', 'description': 'The publication date of the book', 'example': '1951-07-16', 'additionalProperties': {'format': 'date'}}
-    ],
-    'required': [
-        {'name': 'title'},
-        {'name': 'author'}
-    ],
-    'description': 'A book in the library.'
-}
+    path_object_context = {
+        'path': '/books',
+        'httpMethod': 'post',
+        'summary': 'Create a new book',
+        'description': 'Creates a new book in the library',
+        'operationId': 'createBook',
+        'tags': ['Books'],
+        'parameters': [
+            {
+                'parameterName': 'bookId',
+                'name': 'id',
+                'in': 'path',
+                'description': 'The unique identifier of the book',
+                'required': True,
+                'type': 'string',
+                'example': 'b1c4d2e5',
+                'parameterFile': os.path.join('parameters', 'bookId.yaml')
+            }
+        ],
+        'requestBodyDescription': 'Book object to be created',
+        'requestBodyRequired': True,
+        'mediaType': 'application/json',
+        'schemaName': 'Book',
+        'schemaFile': os.path.join('schemas', 'book.yaml'),
+        'statusCode': '201',
+        'responseDescription': 'Book successfully created',
+    }
 
-generate_schema(schema_context, 'generated_schema.yaml')
+    generate_path_object(path_object_context, os.path.join(f"{project_name}/paths", 'books.yaml'), version='v3.0')
 
-path_object_context = {
-    'path': '/books',
-    'httpMethod': 'post',
-    'summary': 'Create a new book',
-    'description': 'Creates a new book in the library',
-    'operationId': 'createBook',
-    'tags': [{'tag': 'Books'}],
-    'parameters': [],
-    'requestBodyDescription': 'Book object to be created',
-    'requestBodyRequired': True,
-    'mediaType': 'application/json',
-    'schemaName': 'Book',
-    'statusCode': '201',
-    'responseDescription': 'Book successfully created',
-}
+    parameter_context = {
+        'parameterName': 'bookId',
+        'name': 'id',
+        'in': 'path',
+        'description': 'The unique identifier of the book',
+        'required': True,
+        'type': 'string',
+        'example': 'b1c4d2e5',
+    }
 
-generate_path_object(path_object_context, 'generated_path_object.yaml')
+    generate_parameter(parameter_context, os.path.join(f"{project_name}/parameters", 'bookId.yaml'), version='v3.0')
 
-parameter_context = {
-    'parameterName': 'bookId',
-    'name': 'id',
-    'in': 'path',
-    'description': 'The unique identifier of the book',
-    'required': True,
-    'type': 'string',
-    'example': 'b1c4d2e5',
-}
+    info_context = {
+        'title': 'Library API',
+        'description': 'An API to manage books and shelves in a digital library',
+        'version': '1.0.0',
+        'contact': {
+            'name': 'Library API Support',
+            'url': 'https://example.com/support',
+            'email': 'support@example.com'
+        },
+        'license': {
+            'name': 'Apache 2.0',
+            'url': 'https://www.apache.org/licenses/LICENSE-2.0'
+        }
+    }
 
-generate_parameter(parameter_context, 'generated_parameter.yaml')
+    generate_info(info_context, os.path.join('info.yaml'), version='v3.0')
 
-info_context = {
-    'title': 'Library API',
-    'description': 'An API to manage books and shelves in a digital library',
-    'version': '1.0.0',
-    'contactName': 'Library API Support',
-    'contactUrl': 'https://example.com/support',
-    'contactEmail': 'support@example.com',
-    'licenseName': 'Apache 2.0',
-    'licenseUrl': 'https://www.apache.org/licenses/LICENSE-2.0',
-}
-
-generate_info(info_context, 'generated_info.yaml')
-
+demo()
