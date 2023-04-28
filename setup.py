279b9ecfd20bb1592c5d6223fbbc5b9b9d748f15
Jmfwolf <77870544+Jmfwolf@users.noreply.github.com>
2023-04-27 19:16:04 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="spec_pilot",
-    version="0.2.0",
+    version="0.2.1",
     author="jmfwolf",
     author_email="jmfwolf@hacksomniac.com",
     description="A tool to generate OpenAPI specifications using natural language",
