9090b0c57afec57f0d6747e66bfe42165d483986
Blake Ramsdell <blaker@gmail.com>
2023-05-09 21:57:21 -0700

    Fix executable name (#11)

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     install_requires=requirements,
     entry_points={
         "console_scripts": [
-            "spec-pilo=spec_pilot.main:main",
+            "spec-pilot=spec_pilot.main:main",
         ],
     },
     classifiers=[
